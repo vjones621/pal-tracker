ec3ff819c64f7424b06b8e16fbd9e8e862fae50d
vanleesa.jones@fedex.com <SkyChp01#>
2021-01-18 23:45:42 +0000

    Lab 2

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    String welcome;
+
+    public WelcomeController(@Value("${welcome.message}") String welcome) {
+        this.welcome = welcome;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcome;
     }
 }
