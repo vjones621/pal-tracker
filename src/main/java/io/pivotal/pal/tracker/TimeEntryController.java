7a64a4a8620df517b339f728f318803c38691ebb
vanleesa.jones@fedex.com <SkyChp01#>
2021-01-20 00:55:01 +0000

    Lab 4

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -0,0 +1,58 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TimeEntryController {
+    private TimeEntryRepository timeEntryRepository;
+
+    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository = timeEntryRepository;
+    }
+
+    @PostMapping("/time-entries")
+    public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntryToCreate) {
+        TimeEntry timeEntry = timeEntryRepository.create(timeEntryToCreate);
+        return ResponseEntity.created(null).body(timeEntry);
+    }
+
+
+    @GetMapping("/time-entries/{timeEntryId}")
+    public ResponseEntity<TimeEntry> read(@PathVariable long timeEntryId) {
+        TimeEntry timeEntry = timeEntryRepository.find(timeEntryId);
+
+        if (timeEntry != null) {
+            return ResponseEntity.ok(timeEntry);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/time-entries")
+    public ResponseEntity<List<TimeEntry>> list() {
+        List<TimeEntry> list = timeEntryRepository.list();
+        return ResponseEntity.ok(list);
+    }
+
+    @PutMapping("/time-entries/{timeEntryId}")
+    public ResponseEntity<TimeEntry> update(@PathVariable long timeEntryId, @RequestBody TimeEntry timeEntryToUpdate) {
+        TimeEntry timeEntry = timeEntryRepository.update(timeEntryId, timeEntryToUpdate);
+
+        if(timeEntry != null){
+            return ResponseEntity.ok(timeEntry);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    @DeleteMapping("/time-entries/{timeEntryId}")
+    public ResponseEntity<Void> delete(@PathVariable long timeEntryId) {
+        timeEntryRepository.delete(timeEntryId);
+        return ResponseEntity.noContent().build();
+    }
+}
